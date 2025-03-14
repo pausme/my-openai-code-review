c77cea03a6012a36fc1d03d75490c374472ac9ff
hsh <zx2086709099@163.com>
2025-03-14 20:13:12 +0800

    feat: maven-jar

--- my-openai-code-review-sdk/src/main/java/com/huang/middleware/sdk/OpenAiCodeReview.java
+++ my-openai-code-review-sdk/src/main/java/com/huang/middleware/sdk/OpenAiCodeReview.java
@@ -1,5 +1,9 @@
 package com.huang.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 /**
  * @Description: openai代码评审执行类
  * @Version: 1.0
@@ -7,7 +11,26 @@ package com.huang.middleware.sdk;
  * @Date: 2025/3/13
  */
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("测试执行");
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
 }
